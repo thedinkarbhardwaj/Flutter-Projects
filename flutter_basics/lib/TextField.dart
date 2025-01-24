7968eb229b12b68d393fb95722ab6ba9855c9c31
thedinkarbhardwaj <dinkar3636@gmail.com>
2025-01-24 18:15:16 +0530

    TextField

---
+++ flutter_basics/lib/TextField.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextFild extends StatefulWidget {
+  const TextFild({super.key});
+
+  @override
+  State<TextFild> createState() => _TextFildState();
+}
+
+class _TextFildState extends State<TextFild> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+
+    );
+  }
+}
