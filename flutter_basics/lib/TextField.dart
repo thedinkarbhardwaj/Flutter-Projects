98442dc0e8b0a53640bf7a1a6f37f187fac454c2
thedinkarbhardwaj <dinkar3636@gmail.com>
2025-02-07 18:39:25 +0530

    Textfield

--- flutter_basics/lib/TextField.dart
+++ flutter_basics/lib/TextField.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+
+void main(){
+  runApp(TextFild());
+}
+
 class TextFild extends StatefulWidget {
   const TextFild({super.key});
 
@@ -7,11 +12,66 @@ class TextFild extends StatefulWidget {
   State<TextFild> createState() => _TextFildState();
 }
 
+
 class _TextFildState extends State<TextFild> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("TextField"),
+          centerTitle: true,
+          backgroundColor: Colors.black12,
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField (
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 2
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                      )
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      )
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
 
+                    ),
+                    suffixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.remove_red_eye)),
+                    prefixIcon: Icon(Icons.email, color: Colors.black,)
+                  ),
+
+                ),
+               Container(height: 30),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+
+                    )
+                  ),
+
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
